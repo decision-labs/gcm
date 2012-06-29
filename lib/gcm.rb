1860996d294111bad3c04b65615716c218c57d96
Shoaib Burq <saburq@gmail.com>
2012-06-29 14:16:20 +0200

    make the api_key required when initialising the class

--- lib/gcm.rb
+++ lib/gcm.rb
@@ -6,21 +6,12 @@ class GCM
   include HTTParty
   default_timeout 30
 
-  attr_accessor :timeout, :auth_token
+  attr_accessor :timeout, :api_key
 
   PUSH_URL = 'https://android.googleapis.com/gcm/send'
 
-  class << self
-    attr_accessor :auth_token
-
-    def build_post_body(registration_ids, options={})
-      body = {}
-      #raise exception if options[:time_to_live] && !options[:collapse_key]
-    end
-  end
-
-  def initialize(auth_token = nil)
-    @auth_token = auth_token || self.class.auth_token
+  def initialize(api_key)
+    @api_key = api_key
   end
 
   # {
@@ -33,7 +24,7 @@ class GCM
   #     "time": "15:10"
   #   }
   # }
-  # gcm = GCM.new(auth_token)
+  # gcm = GCM.new(api_key)
   # gcm.send_notification({registration_ids: ["4sdsx", "8sdsd"], data: {score: "5x1"}})
   def send_notification(registration_ids, options)
     post_body = self.class.build_post_body(registration_ids, options)
@@ -41,12 +32,19 @@ class GCM
     params = {
       :body    => post_body,
       :headers => {
-        'Authorization'  => "key=#{@auth_token}",
+        'Authorization'  => "key=#{@api_key}",
         'Content-type'   => 'application/json',
       }
     }
 
     self.class.post(PUSH_URL, params)
+  end
+
+  private
+
+  def build_post_body(registration_ids, options={})
+    body = {}
+    #raise exception if options[:time_to_live] && !options[:collapse_key]
   end
 
 end
