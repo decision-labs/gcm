7a5e0b300f4dab3611f40e1628e1e6754a4e6b46
Kashif Rasul <kashif.rasul@gmail.com>
2014-01-09 23:50:05 +0100

    fixed rspec depreication warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,9 @@ require 'webmock/rspec'
 require 'gcm'
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
   # config.filter_run :focus
 end
